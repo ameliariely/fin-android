4b5eadd9616815af97365aa171e1b6c2ac04be9f
Amelia <ariely@etsy.com>
2017-04-28 10:02:01 -0400

    Made a button

---
+++ app/src/main/java/fin/fin/FragmentConnect.java
@@ -0,0 +1,20 @@
+package fin.fin;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ariely on 04/28/2017.
+ */
+
+public class FragmentConnect extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_connect, container, false);
+    }
+}
--- app/src/main/java/fin/fin/MainActivity.java
+++ app/src/main/java/fin/fin/MainActivity.java
@@ -1,7 +1,9 @@
 package fin.fin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View containerView = findViewById(R.id.main_activity_layout);
+
+        if (savedInstanceState == null) {
+            Fragment newFragment = new FragmentConnect();
+            android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(containerView.getId(), newFragment).commit();
+        }
     }
 }
